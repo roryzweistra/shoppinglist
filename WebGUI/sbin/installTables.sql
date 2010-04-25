cb0555eb1abac9cc8982636b1bca27c2318240f8
Oqapi <developers@oqapi.nl>
2010-04-25 20:16:21 +0200

    backtick errories

--- WebGUI/sbin/installTables.sql
+++ WebGUI/sbin/installTables.sql
@@ -5,8 +5,8 @@ create table `shoppingProduct` (
     `barcode` int(13) null,
     `ownerId` char(22) not null,
     `dateAdded` bigint(20) not null,
-	primary key (id),
-    key shoppingProduct (title)
+	primary key (`id`),
+    key shoppingProduct (`title`)
 )
 
 create table `shoppingCategory` (
@@ -14,8 +14,8 @@ create table `shoppingCategory` (
     `title` varchar(255) not null,
     `ownerId` char(22) not null,
     `dateAdded` bigint(20),
-    primary key (id),
-    key shoppingCategoryTitle (title)
+    primary key (`id`),
+    key shoppingCategoryTitle (`title`)
 )
 
 create table `shoppingStore` (
@@ -23,8 +23,8 @@ create table `shoppingStore` (
     `title` varchar(255) not null,
     `ownerId` char(22) not null,
     `dateAdded` bigint(20),
-    primary key (id),
-    key shoppingStoreTitle (title)
+    primary key (`id`),
+    key shoppingStoreTitle (`title`)
 )
 
 create table `shoppingStoreArea` (
@@ -34,8 +34,8 @@ create table `shoppingStoreArea` (
     `shoppingStoreId` char(22) not null,
     `dateAdded` bigint(20) not null,
     `rank` int(3),
-    primary key (id),
-    key shoppingStoreAreaTitle (title)
+    primary key (`id`),
+    key shoppingStoreAreaTitle (`title`)
 )
 
 create table `shoppingList` (
@@ -46,8 +46,8 @@ create table `shoppingList` (
     `defaultSort` varchar(255) null,
     `completed` smallint(1) default 0,
     `templateId` char(22)
-    primary key (id),
-    key shoppingListTitle (title)
+    primary key (`id`),
+    key shoppingListTitle (`title`)
 )
 
 create table `shoppingListProduct` (
@@ -58,5 +58,5 @@ create table `shoppingListProduct` (
     `shoppingProductId` char(22) not null,
     `shoppingStoreId` char(22) null,
     `quantity` int(3) not null default 1,
-    primary key (id),
+    primary key (`id`),
 )
